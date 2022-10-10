1c72cb757ba9eed93f1bdebd1bf16012c6617868
Anwar <stormyshadow2009@outlook.com>
2022-10-10 23:20:16 +0200

    adding resources to navbar

--- components/header/navbar.tsx
+++ components/header/navbar.tsx
@@ -10,6 +10,7 @@ function Navbar() {
       <h1 className="p-4 m-4 text-center border-b-2">ANWAR AHMED</h1>
       <nav className="flex flex-row justify-around mt-5  flex-wrap">
           <Link href="#about-section" className="p-2 m-2"><a >ABOUT</a></Link>
+          <Link href="#resources-section" className="p-2 m-2"><a >RESOURCES</a></Link>
           <Link href="#skills-section" className="p-2 m-2"><a >SKILLS</a></Link>
           <Link href="#projects-section" className="p-2 m-2"><a >PROJECTS</a></Link>
           <Link href="#contact-section" className="p-2 m-2"><a >CONTACT</a></Link>
