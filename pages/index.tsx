b659dc9d1d420f28b874d2856810cf57f8849c32
Anwar <stormyshadow2009@outlook.com>
2022-10-12 16:55:00 +0200

    adding new favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,11 @@ const Home: NextPage = () => {
       <Head>
         <title>WEB DEV ANWAR</title>
         <meta name="description" content="my portfolio app content about me as a web developer" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <HeaderContainer />
       <MainContainer />
