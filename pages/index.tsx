dfee89e35ec7898390c7bf9243f574f601bcc47b
Anwar <stormyshadow2009@outlook.com>
2022-10-14 00:58:34 +0200

    done with favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,12 +12,12 @@ const Home: NextPage = () => {
   return (
     <div className= "text-white bg-space-img bg-cover flex flex-col items-center font-mono">
       <Head>
-        <title>WEB DEV ANWAR</title>
+        <title>Anwar WEB DEVELOPER</title>
         <meta name="description" content="my portfolio app content about me as a web developer" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <HeaderContainer />
