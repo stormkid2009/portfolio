bc01d602b254b312cf1118b4efd96b04d4a47664
Anwar <stormyshadow2009@outlook.com>
2022-10-20 22:35:32 +0200

    change path to suit developping mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,10 @@ const Home: NextPage = () => {
       <Head>
         <meta name="description" content="my portfolio app content about me as a web developer" />
         
-        <link rel="apple-touch-icon" sizes="180x180" href="/portfolio/favicon_io/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/portfolio/favicon_io/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/portfolio/favicon_io/favicon-16x16.png" />
-        <link rel="manifest" href="/portfolio/favicon_io/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
       </Head>
       <HeaderContainer />
       <MainContainer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: theme => ({
-                'hero-img': "url('/portfolio/sherif.jpg')",
-                'space-img': "url('/portfolio/space.jpg')",
+                'hero-img': "url('/sherif.jpg')",
+                'space-img': "url('/space.jpg')",
                 })
     },
     colors:{
